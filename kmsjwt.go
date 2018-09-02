1af97a24946fe0adef13b941fe0bdc392aec378a
Marcin Wyszynski <marcin.pixie@gmail.com>
2018-09-02 12:41:13 +0200

    Initial commit

---
+++ kmsjwt.go
@@ -0,0 +1,99 @@
+package kmsjwt
+
+import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/service/kms/kmsiface"
+	"github.com/dgrijalva/jwt-go"
+)
+
+const kmsAlgorighm = "KMS"
+
+// ErrKmsVerification is an error shown when KMS token verification fails.
+var ErrKmsVerification = errors.New("kms: verification error")
+
+type kmsClient struct {
+	kmsiface.KMSAPI
+	cache    map[string]string
+	kmsKeyID string
+}
+
+// Option is a function that modifies the way the verification method works.
+type Option func(*kmsClient)
+
+// DisableCache disables cache on the client (on by default).
+func DisableCache(k *kmsClient) {
+	k.cache = nil
+}
+
+// New provides a KMS-based implementation of JWT signing method.
+func New(client kmsiface.KMSAPI, kmsKeyID string, opts ...Option) jwt.SigningMethod {
+	return &kmsClient{
+		KMSAPI:   client,
+		cache:    make(map[string]string),
+		kmsKeyID: kmsKeyID,
+	}
+}
+
+func (k *kmsClient) Alg() string {
+	return kmsAlgorighm
+}
+
+func (k *kmsClient) Sign(signingString string, _ interface{}) (string, error) {
+	checksum := sha256Checksum(signingString)
+	input := &kms.EncryptInput{KeyId: aws.String(k.kmsKeyID), Plaintext: checksum}
+	output, err := k.Encrypt(input)
+	if err != nil {
+		return "", jwt.ErrInvalidKey
+	}
+	return base64.StdEncoding.EncodeToString(output.CiphertextBlob), nil
+}
+
+func (k *kmsClient) Verify(signingString, providedSignature string, _ interface{}) error {
+	checksum := sha256Checksum(signingString)
+	if k.verifyCache(signingString, providedSignature, checksum) {
+		return nil
+	}
+	ciphertext, err := base64.StdEncoding.DecodeString(providedSignature)
+	if err != nil {
+		return err
+	}
+	output, err := k.Decrypt(&kms.DecryptInput{CiphertextBlob: ciphertext})
+	if err != nil {
+		return ErrKmsVerification
+	}
+	// The output keyID is the full ARN, the internal ID is just the key ID.
+	if !strings.HasSuffix(*output.KeyId, k.kmsKeyID) {
+		return jwt.ErrInvalidKey
+	}
+	if subtle.ConstantTimeCompare(output.Plaintext, checksum) != 1 {
+		return ErrKmsVerification
+	}
+	if k.cache != nil {
+		k.cache[signingString] = providedSignature
+	}
+	return nil
+}
+
+func (k *kmsClient) verifyCache(signingString, providedSignature string, checksum []byte) bool {
+	if k.cache == nil {
+		return false
+	}
+	signature, ok := k.cache[signingString]
+	if ok || signature != providedSignature {
+		return false
+	}
+	subtle.ConstantTimeCompare(checksum, checksum)
+	return true
+}
+
+func sha256Checksum(signingString string) []byte {
+	ret := sha256.Sum256([]byte(signingString))
+	return ret[:]
+}
---
+++ kmsjwt_test.go
@@ -0,0 +1,126 @@
+package kmsjwt
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/service/kms/kmsiface"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type mockKMS struct {
+	mock.Mock
+	kmsiface.KMSAPI
+}
+
+func (m *mockKMS) Decrypt(input *kms.DecryptInput) (*kms.DecryptOutput, error) {
+	args := m.Called(input)
+	return args.Get(0).(*kms.DecryptOutput), args.Error(1)
+}
+
+func (m *mockKMS) Encrypt(input *kms.EncryptInput) (*kms.EncryptOutput, error) {
+	args := m.Called(input)
+	return args.Get(0).(*kms.EncryptOutput), args.Error(1)
+}
+
+type KMSImplementationTestSuite struct {
+	suite.Suite
+	mockAPI *mockKMS
+	keyID   string
+	sut     jwt.SigningMethod
+}
+
+func (s *KMSImplementationTestSuite) SetupTest() {
+	s.mockAPI = new(mockKMS)
+	s.keyID = "kms key ID"
+	s.sut = New(s.mockAPI, s.keyID)
+}
+
+func (s *KMSImplementationTestSuite) TestAlg() {
+	s.Equal("KMS", s.sut.Alg())
+}
+
+func (s *KMSImplementationTestSuite) TestSignOK() {
+	s.mockAPI.On("Encrypt", mock.MatchedBy(func(input interface{}) bool {
+		encryptInput, ok := input.(*kms.EncryptInput)
+		if !ok {
+			return false
+		}
+		if *encryptInput.KeyId != s.keyID {
+			return false
+		}
+		if fmt.Sprintf("%x", encryptInput.Plaintext) != "96d62e2abd3e42de5f50330fb8efc4c5599835278077b21e9aa0b33c1df07a1c" {
+			return false
+		}
+		return true
+	})).Return(&kms.EncryptOutput{
+		CiphertextBlob: []byte("secret"),
+		KeyId:          aws.String(s.keyID),
+	}, nil)
+	ret, err := s.sut.Sign("plaintext", nil)
+	s.NoError(err)
+	s.Equal("c2VjcmV0", ret)
+}
+
+func (s *KMSImplementationTestSuite) TestSignError() {
+	s.mockAPI.On(
+		"Encrypt",
+		mock.AnythingOfType("*kms.EncryptInput"),
+	).Return((*kms.EncryptOutput)(nil), errors.New("bacon"))
+	ret, err := s.sut.Sign("plaintext", nil)
+	s.EqualError(err, "key is invalid")
+	s.Empty(ret)
+}
+
+func (s *KMSImplementationTestSuite) TestVerifyOK() {
+	plaintext, err := base64.StdEncoding.DecodeString("ve/CWUJr0IbREmEIrDlVpsFRZMofWR4Icux8SeRALmo=")
+	s.NoError(err)
+	s.mockAPI.On(
+		"Decrypt",
+		mock.MatchedBy(func(input interface{}) bool {
+			decryptInput, ok := input.(*kms.DecryptInput)
+			if !ok {
+				return false
+			}
+			if string(decryptInput.CiphertextBlob) != "bacon\n" {
+				return false
+			}
+			return true
+		}),
+	).Return(&kms.DecryptOutput{
+		KeyId:     aws.String(s.keyID),
+		Plaintext: plaintext,
+	}, nil)
+	s.NoError(s.sut.Verify("signing", "YmFjb24K", nil))
+}
+
+func (s *KMSImplementationTestSuite) TestVerifyDecryptFails() {
+	s.mockAPI.
+		On("Decrypt", mock.AnythingOfType("*kms.DecryptInput")).
+		Return((*kms.DecryptOutput)(nil), errors.New("bacon"))
+	s.EqualError(s.sut.Verify("signing", "YmFjb24K", nil), "kms: verification error")
+}
+
+func (s *KMSImplementationTestSuite) TestVerifyDifferentKey() {
+	s.mockAPI.
+		On("Decrypt", mock.AnythingOfType("*kms.DecryptInput")).
+		Return(&kms.DecryptOutput{KeyId: aws.String("huh?")}, nil)
+	s.EqualError(s.sut.Verify("signing", "YmFjb24K", nil), "key is invalid")
+}
+
+func (s *KMSImplementationTestSuite) TestVerifyDoesNotCompare() {
+	s.mockAPI.
+		On("Decrypt", mock.AnythingOfType("*kms.DecryptInput")).
+		Return(&kms.DecryptOutput{KeyId: aws.String(s.keyID), Plaintext: []byte("wrong")}, nil)
+	s.EqualError(s.sut.Verify("signing", "YmFjb24K", nil), "kms: verification error")
+}
+
+func TestKMSImplementation(t *testing.T) {
+	suite.Run(t, new(KMSImplementationTestSuite))
+}
